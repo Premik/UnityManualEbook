f66738b005f8400231018d350921f16906f622ef
Premik <premysl.srubar@post.cz>
2018-05-03 12:25:51 +0200

    Doc

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Unity manual to eBook
 
+Steps and a script to reformat the Unity3d user manual to an eBook-friendly html file.
+
 ![UnityManualInKindle](doc/UnityManualInKindle.jpg)
 
-## Steps
+## Download the offline manual
 
-### Download the manual
-
-Download the offline documentation from the unity3d page. For example: [2018.2](https://docs.unity3d.com/2018.2/Documentation/Manual/OfflineDocumentation.html).
+Download the offline documentation from the unity3d page. For example: [2018.2](https://docs.unity3d.com/2018.2/Documentation/Manual/OfflineDocumentation.html)
 
 ```bash
  wget https://docs.unity3d.com/2018.1/Documentation/uploads/UnityDocumentation.zip
@@ -14,11 +14,11 @@ Download the offline documentation from the unity3d page. For example: [2018.2](
  cd /tmp/unity/Documentation/en
 ```
 
-### Convert images
+## Convert images
 
 **This is an optional step**. Not needed if you are using a device with full capable web browser.
 
-There are `jpg`, `png`, `gif`, `svg` and `pdf` files included in the manual. I wanted all the images to be grayscaled png files for my Kindle reader.
+There are `jpg`, `png`, `gif`, `svg` and `pdf` files included in the manual. I wanted all the images to be grayscale png files for my Kindle reader.
 
 Convert `svg` with the [Inkscape](https://inkscape.org):
 
@@ -32,8 +32,8 @@ Convert `jpg` with [ImageMagic's](https://www.imagemagick.org/script/index.php) 
 find . -name "*.jpg" -exec convert -verbose {} {}.png \;
 ```
 
-The same for the `gif`s. Since the gifs are animated the `convert` will create multiple numbered `png` 
-files for each frame. The groovy script can deal with this but it takes only one frame from the animation.
+The same for the `gif`s. Since the `gif` files are animated the `convert` will create multiple numbered `png` 
+files for each frame. The Groovy script can deal with this but it takes only one frame from the animation.
 
 ```bash
 find . -name "*.gif"  -exec convert -verbose {} {}.png \;
@@ -46,46 +46,45 @@ find . -name "*.png" -exec pngcrush -brute -l 9 -rem gAMA -rem cHRM -rem iCCP -r
 ```
 
 As an alternative the `mogrify` from the ImageMagic can be used too. But it would produce bigger images.
-The the `pngcrush` is slow. It takes more than 30 minutes to convert all the pngs on an average pc. But the resulted images (after bit-depth reduction and grayscale) are `3x-5x` times smaller.
+The the `pngcrush` is slow. It takes more than 30 minutes to convert all the `pngs` on an average pc. But the resulted images (after bit-depth reduction and grayscale) are `3x-5x` times smaller.
 
 ```bash
 #find . -name "*.png" -exec mogrify -verbose -type Grayscale -depth 8 -quality 9 -format png {}  \;
 ```
 
 Note all the above commands (except the last one ) will produce a copy of the file with multiple extensions.
-Like `foobar.svg.png.png`. The groovy script updates all the image links while prefering the longest file name.
+Like `foobar.svg.png.png`. The Groovy script updates all the image links while prefering the longest file name.
 So the `foobar.svg.png.png` will be used instead of the original `foobar.svg`
 
 ![Pngs](doc/Pngs.png)
 
-### The Groovy Script
+## Run the Groovy script
 
 The `ManualProcessor.groovy` script:
 
 - Parses the TOC file to recognize order of the pages .
 - Linearizes and removes unnecessary content from each page.
-- Concacenate multiple pages into single html file.
-- For each first level chapter creates a new html file.
+- Concatenate multiple pages into single `html` file.
+- For each first level chapter creates a new `html` file.
 - Makes sure there are not too many pages in single chapters file.
 - Add number prefix for each file so the order is obvious.
 - Unsure the img links are correct after above image conversation.
 
-#### Steps
 
-- Get the script. Clone this repo or just: 
+To get the script simply clone this repo or just:
 
 ```bash
 wget https://raw.githubusercontent.com/Premik/UnityManualEbook/master/ManualProcessor.groovy 
 ```
 
 - Get and install [Groovy](http://groovy-lang.org/)
-- Review the grovy script. Currently `maxSubChaptersInOneFile` is set to `12`.
+- Review the Groovy script. At the top there is `Param` class. If you are going to use a web-browser for reading set the `removeLocalUrlLinks = false` to keep the local links.
 - Run the script inside the unziped folder `Documentation/en/`:
 
 ```bash
 groovy UnityManual.groovy
 ```
-It will produce new **Manual-flattened** with the linearized html files:
+It will produce a new **Manual-flattened** folder with the linearized html files beside the original 'Manul' folder:
 
  * `Manual`
  * **Manual-flattened**
@@ -99,3 +98,10 @@ It will produce new **Manual-flattened** with the linearized html files:
  * `StaticFiles`
  * `StaticFilesManual`
  * `upload`
+
+ ## Calibre
+- In the [Calibre](https://calibre-ebook.com/) import all the `html` files as individual books. Or just drag them to the library view.
+- Convert them to the desired format. I used `AZW3` aka `KF8` for my Kindle as the Mobi format had problems placing the images on proper location. 
+- The total size of the User Manul in the `AZW3` format in may case was slightly over `90MB`.
+## Limitation
+- Current the process doesn't work for the ScriptingManual. Probably the html structure is slightly different to the User Manual one so the script removes too much. Perhaps I'll fix this once I'm done reading the User manual..
--- UnityManual.groovy
+++ UnityManual.groovy
@@ -1,8 +1,5 @@
 #!/bin/groovy
-@Grapes([
-	@Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
-	@Grab(group='ch.qos.logback', module='logback-classic', version='1.1.7')
-])
+@Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14')
 
 import org.cyberneko.html.parsers.SAXParser
 import groovy.json.JsonSlurper
@@ -12,13 +9,32 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 
-@Slf4j
+class Params {
+	/** Max number of original pages to fit into a single file. This affect number of file 'parts' generated  */
+	int maxSubChaptersInOneFile = 24
+
+	/**
+	 * Remove all <a href="url"> where the url is not a 'http://' link. 
+	 * This is needed for Calibre to not include all the linked objects (anohter html, pdfs) into the single book. 
+	 */
+	boolean removeLocalUrlLinks = true
+
+	/** Amount of console output. -1 .. 2 */
+	int verbose = 0
+
+	/** Location of the unity manual. */
+	String rootPath = "."
+
+	/**
+	 * Which subfolder to convert. 
+	 *  'Manual' - the User manual.
+	 *  'ScriptReference' - the script reference (currently doesn't work well) 
+	 */
+	String subFolderName = "Manual"
+}
+
 class HtmlProcessor {
-	
-	def maxSubChaptersInOneFile = 6
-	def removeLocalUrlLinks = true
-	
-	
+
 	def toc = [:]
 	private String outputDir
 	private String rootDir
@@ -29,10 +45,31 @@ class HtmlProcessor {
 	private Map<String, Object> currentChapter
 	private Writer writter
 	private String rootChapterName
-	
-	public HtmlProcessor(String rootPath, String subFolderName) {
-		rootDir = "$rootPath/$subFolderName"
-		outputDir = "$rootPath/$subFolderName-flattened"
+	private Params p
+
+	private void trace(String msg) {
+		if (p.verbose>=2) println(msg)
+	}
+	private void debug(String msg) {
+		if (p.verbose>=1) println(msg)
+	}
+	private void info(String msg) {
+		if (p.verbose>=0) println(msg)
+	}
+	private void error(String msg) {
+		System.err.println(msg)
+	}
+
+	public HtmlProcessor(Params params) {
+		p = params
+		p.rootPath = new File(p.rootPath).getCanonicalFile().toString()
+		rootDir = "$p.rootPath/$p.subFolderName"
+		outputDir = "$p.rootPath/$p.subFolderName-flattened"
+		if (!new File(rootDir).exists()) {
+			def m = "The $rootDir doesn't exists. Please copy this script into the 'Documentation/en/' of the unity manual"
+			error(m)
+			throw new IllegalArgumentException(m)
+		}
 	}
 
 	public String getTitle() {
@@ -49,29 +86,29 @@ class HtmlProcessor {
 		return ch
 	}
 
-	
+
 	private void openNewWritter() {
-		if (writter) closeWritter()		
+		if (writter) closeWritter()
 		String ch = rootChapterCounter.toString().padLeft(2, '0')
 		String part = ""
 		part = "part${('A'..'Z')[filePartCounter]}-"
 		File f = new File("$outputDir/$ch-$rootChapterName-$part${link}.html")
 		chaptersInFilePart = 0
 		writter = f.newWriter('UTF-8')
-		
+
 		writter.write(
-			$/<!DOCTYPE html><html lang="en">
+				$/<!DOCTYPE html><html lang="en">
 				<head>
 				<meta charset=utf-8><title>$rootChapterName</title>
 			</head><body>
 		/$)
-		
+
 		//writter.write('<?xml version="1.0"?>')
 		//writter.write('<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head>')
 		//writter.write('<meta charset="UTF-8">')
 		//writter.write("<title>$rootChapterName</title></head><body>\n")
-		
-		
+
+
 	}
 
 	private void closeWritter() {
@@ -85,18 +122,18 @@ class HtmlProcessor {
 	private void loadToc() {
 		String tocFile = "$rootDir/docdata/toc.json"
 
-		log.trace("Loading toc $tocFile")
+		trace("Loading toc $tocFile")
 		def jsonSlurper = new JsonSlurper()
 		toc = jsonSlurper.parse(new File(tocFile))
-		toc.children.each { log.debug("Chapter:  ${it.title.trim()} [${it.link} ${it.children?.size()}] ")}
-		log.debug('-'*40)
+		toc.children.each { debug("Chapter:  ${it.title.trim()} [${it.link} ${it.children?.size()}] ")}
+		debug('-'*40)
 		new File(outputDir).mkdir()
 		chaptersInFilePart = -1
 		rootChapterCounter=0
 	}
 
 	private String GPathToString(GPathResult node) {
-		
+
 		def builder = groovy.xml.StreamingMarkupBuilder.newInstance();
 		Writable w = builder.bindNode(node)
 		String ret = w.toString() +"\n"
@@ -116,16 +153,16 @@ class HtmlProcessor {
 		def slurper = new XmlSlurper(parser)
 		def file = new File("$rootDir/${link}.html")
 		if (!file.exists()) {
-			log.error("$file not found. Ignoring link. Current article: $title")
+			error("$file not found. Ignoring link. Current article: $title")
 			return
 		}
 		def html = slurper.parse(file)
-		
+
 		//Find the <div class="section/>
 		GPathResult sectionDiv = html.'**'.findAll { it.@class == 'section'}.first()
-		
+
 		sectionDiv.DIV.replaceNode {} //Remove all child <div>s
-		
+
 		//Remove page-edit history
 		GPathResult[] pageEditSpan = sectionDiv.'**'.findAll { it.@class in ['page-edit', 'page-history']}
 		pageEditSpan.each {
@@ -135,12 +172,19 @@ class HtmlProcessor {
 			} else {//Sometimes it is just plain p
 				it.parent().replaceNode {}
 			}
-			
+
 		}
-		
+
 		def imgs = sectionDiv.'**'.findAll { it.name() == 'IMG'}
 		processImages(imgs)
-			
+		//Find all hrefs with local href
+		def hrefs = sectionDiv.'**'.findAll { it.name() == 'A' && !"${it.@href}".startsWith('http://')}
+		hrefs.each {
+			//Remove the url (change to link the manual online?)
+			it.@href = ""
+		}
+
+
 		writter.write(GPathToString(sectionDiv))
 
 		//throw new RuntimeException()
@@ -149,30 +193,36 @@ class HtmlProcessor {
 
 	private processImages(imgs) {
 		imgs.each {img->
-			
+
 			String imgSrc = "$outputDir/${img.@src}"
 			File imgFile = new File(imgSrc).getCanonicalFile()
 			String imgBaseName = imgFile.name
-			def exts = ['.svg', '.jpg', '.jpeg', '.png', '.gif']
+			def exts = [
+				'.svg',
+				'.jpg',
+				'.jpeg',
+				'.png',
+				'.gif'
+			]
 			//Remove ext (s)
 			exts.each { imgBaseName = imgBaseName.replaceAll(it, '') }
 
 
-			//Find all files which start with the original name in the link
+			//Find all existing files which start with the original name in the link
 			def fk = new FileNameFinder().getFileNames(imgFile.parent, "$imgBaseName*")
 			if (!fk) {
-				log.error("${img.@src} doesn't exist")
+				error("${img.@src} doesn't exist")
 				return //Continue
 			}
 			//Take the longest filename like foo.svg.png. Assume it is the most processed one
 			def newSrc = new File(fk.max { it.size() }).getCanonicalFile()
-			
+
 			if (newSrc == imgFile) return //The same, ignore
-			//Relativize the new link			
-			Path outputDirPath = Paths.get(outputDir)
+				//Relativize the new link
+				Path outputDirPath = Paths.get(outputDir)
 			Path newPath = outputDirPath.relativize(newSrc.toPath())
 			img.@src =newPath.toString()
-			log.debug("Corrected img link $imgFile.name to  $newPath")
+			trace("Corrected img link $imgFile.name to  $newPath")
 		}
 	}
 
@@ -180,14 +230,14 @@ class HtmlProcessor {
 	private void processChapter(int deep) {
 		if (deep == 0) {//Root chapter
 			rootChapterName = link
-			log.info("${'-'*10} $title ${'-'*10}")
+			info("${'-'*10} $title ${'-'*10}")
 			filePartCounter =0
 			rootChapterCounter++
 			openNewWritter()
 		}
-		log.debug("${' '*deep}$title")
-		
-		if (chaptersInFilePart > maxSubChaptersInOneFile) {
+		debug("${' '*deep}$title")
+
+		if (chaptersInFilePart > p.maxSubChaptersInOneFile) {
 			chaptersInFilePart = 0
 			filePartCounter++
 			openNewWritter()
@@ -204,7 +254,7 @@ class HtmlProcessor {
 		}
 	}
 
-	void run() {		
+	void run() {
 		loadToc()
 		currentChapter = toc
 		processChildChapters()
@@ -214,14 +264,12 @@ class HtmlProcessor {
 
 
 }
-new HtmlProcessor("/tmp/work/Documentation/en", "Manual").run()
 
-if (!new File("Manual").exists()) {
-	System.err.println "There is no 'Manual' folder in the current directly. Please copy this link into the 'Documentation/en/' of the unity manual"
-	return
-}
 
-new HtmlProcessor(new File(".").getCanonicalFile().toString(), "Manual").run()
+def p = new Params()
+
+p.rootPath = "/tmp/work/Documentation/en"
+new HtmlProcessor(p).run()
 //new Navigator("/tmp/work/Documentation/en", "ScriptReference").run()
 
 
